1ef8904d47179049cd852014091cb8a820a38fe1
Cecilia Coutinho <cecicoutinho@gmail.com>
2022-12-13 16:06:19 +0100

    12-array first commit

---
+++ 08-dumbo-octopus/DumboOctopusApp/DumboOctopus/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace DumboOctopus
+{
+    internal class DetectFlashed
+    {
+        static void Main(string[] args)
+        {
+            int[,] octopus = new int[10, 10]
+{
+                { 5, 4, 8, 3, 1, 4, 3, 2, 2, 3},
+                { 2, 7, 4, 5, 8, 5, 4, 7, 1, 1},
+                { 5, 2, 6, 4 ,5, 5, 6, 1, 7 ,3},
+                { 6, 1, 4, 1, 3, 3, 6, 1, 4, 6},
+                { 6, 3, 5, 7, 3, 8, 5, 4, 7, 8},
+                { 4, 1, 6, 7, 5, 2, 4, 6, 4, 5},
+                { 2, 1, 7, 6, 8, 4, 1, 7, 2, 1},
+                { 6, 8, 8, 2, 8, 8, 1, 1, 3, 4},
+                { 4, 8, 4, 6, 8, 4, 8, 5, 5, 4},
+                { 5, 2, 8, 3, 7, 5, 1, 5, 2, 6}
+};
+
+            Console.WriteLine("==========Welcome to Dumbo Octopus!==========");
+            Console.WriteLine("How many steps?: ");
+            Console.Write("");
+            int userInput;
+            int.TryParse(Console.ReadLine(), out userInput);
+            for (int step = 0; step <= userInput; step++)
+            {
+                Console.WriteLine($"\nStep Number: {step + 1}");
+                octopus = NewOctopusForIncreaseEnergy(octopus);
+                var flashed = DetectFlash(octopus);
+                octopus = ResetFlashed(octopus, flashed);
+                PrintBoard(octopus);
+                //foreach (var tuple in flashed)
+                //{
+                //    Console.WriteLine("{0} - {1}", tuple.Item1, tuple.Item2);
+                //}
+            }
+        }
+
+        static int[,] NewOctopusForIncreaseEnergy(int[,] octopus)
+        {
+            int rowSize = octopus.GetLength(0);
+            int colSize = octopus.GetLength(1);
+
+            int[,] newOctopus = new int[rowSize, colSize];
+
+            for (int row = 0; row < rowSize; row++)
+            {
+                for (int col = 0; col < colSize; col++)
+                {
+                    newOctopus[row, col] = octopus[row, col] + 1;
+                }
+            }
+            return newOctopus;
+        }
+
+        static void PrintBoard(int[,] octopus)
+        {
+            int rowSize = octopus.GetLength(0);
+            int colSize = octopus.GetLength(1);
+
+            for (int row = 0; row < rowSize; row++)
+            {
+                for (int col = 0; col < colSize; col++)
+                {
+                    Console.Write(octopus[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //energy level greater than 9 flashes
+        static List<Tuple<int, int>> DetectFlash(int[,] octopus)
+        {
+            int rowSize = octopus.GetLength(0);
+            int colSize = octopus.GetLength(1);
+
+            int[,] newOctopus = new int[rowSize, colSize];
+            var flashedList = new List<Tuple<int, int>>();
+
+            for (int row = 0; row < rowSize; row++)
+            {
+                for (int col = 0; col < colSize; col++)
+                {
+                    if (octopus[row, col] > 9)
+                    {
+                        //flash!
+                        flashedList.Add(new Tuple<int, int>(row, col));
+                    }
+                }
+            }
+            return flashedList;
+        }
+
+        static int[,] ResetFlashed(int[,] octopus, List<Tuple<int, int>> flashed)
+        {
+            int rowSize = octopus.GetLength(0);
+            int colSize = octopus.GetLength(1);
+
+            int[,] newOctopus = new int[rowSize, colSize];
+
+            for (int row = 0; row < rowSize; row++)
+            {
+                for (int col = 0; col < colSize; col++)
+                {
+                    Tuple<int, int> current = new Tuple<int, int>(row, col);
+                    if (flashed.Contains(current))
+                    {
+                        newOctopus[row, col] = 0;
+                    }
+                    else
+                    {
+                        newOctopus[row, col] = octopus[row, col];
+                    }
+                }
+            }
+            return newOctopus;
+        }
+
+        //TO DO: near +1
+        static void FindAdjacent(int[,] octopus)
+        {
+
+        }
+    }
+}
---
+++ 12-array/ArrayApp/Array/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        //Single-Dimensional Arrays
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n\t==================WELCOME!==================\n");
+            Console.ResetColor();
+
+            //declare an array with the user input amount of integers:
+            Console.WriteLine("\n\tHow many integers do you want to store in the array? ");
+            Console.Write("\t ");
+            int amountArray;
+            int.TryParse(Console.ReadLine(), out amountArray);
+
+            if (amountArray > 0)
+            {
+                //declare array values
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\n\tEnter {amountArray} integers: ");
+                Console.ResetColor();
+
+                int counter = 1;
+                while (counter <= amountArray)
+                {
+                    string? userValuesArray = " ";
+
+                    //list to storage user values
+                    List<int> values = new List<int>();
+
+                    for (counter = 1; counter <= amountArray; counter++)
+                    {
+                        Console.Write($"\n\tType {AddOrdinal(counter)} integer: ");
+                        userValuesArray = Console.ReadLine();
+
+                        //to add user values to a list
+                        int aValue;
+                        int.TryParse(userValuesArray, out aValue);
+                        values.Add(aValue);
+                    }
+
+                    //element type of array: integer
+                    int[] userArrayInt = new int[amountArray];
+
+                    //add list values to array
+                    userArrayInt = values.ToArray();
+
+
+                    // print array values
+                    PrintResult(userArrayInt);
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\tERROR!Invalid!" +
+                "\n\tInput needs to be a number and higher than 0\n");
+                Console.ResetColor();
+                Console.WriteLine("\n\tPress ENTER to EXIT\n");
+                Console.ReadLine();
+            }
+        }
+
+        static void PrintResult(int[] userArrayInt)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n\t=========YOUR ARRAY=========");
+            for (int i = 0; i < userArrayInt.Length; i++)
+            {
+                Console.WriteLine($"\n\tIndex {i}: {userArrayInt[i]}");
+            }
+            Console.WriteLine("\n\t============================");
+            Console.ResetColor();
+            Console.WriteLine("\n\tPress ENTER to EXIT\n");
+            Console.ReadLine();
+        }
+
+        //to transform cardinal numbers to ordinal
+        public static string AddOrdinal(int num)
+        {
+            if (num <= 0) return num.ToString();
+
+            switch (num % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return num + "th";
+            }
+
+            switch (num % 10)
+            {
+                case 1:
+                    return num + "st";
+                case 2:
+                    return num + "nd";
+                case 3:
+                    return num + "rd";
+                default:
+                    return num + "th";
+            }
+        }
+    }
+}
