0c7e059f28d57edf225b50e210adeeb6d23f2e64
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-02-21 14:41:19 +0100

    menu added

--- 16-db-csharp/Program.cs
+++ 16-db-csharp/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Workshop16DbCsharp
+﻿using System;
+
+namespace Workshop16DbCsharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            RunMenu();
+        }
+
+        static void RunMenu()
+        {
+            bool runMenu = true;
+
+            while (runMenu)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("\n\tVälkommen till databasen! Vänligen välj ett av nedanstående alternativ:\n");
+                Console.ResetColor();
+
+                Console.WriteLine("\n\t1. Lista studenter\n" +
+                            "\n\t2. Lista kurser\n" +
+                            "\n\t3. Skapa student\n" +
+                            "\n\t4. Skapa kurs\n" +
+                            "\n\t5. Byt lösenord\n" +
+                            "\n\t6. Redigera kurs\n" +
+                            "\n\t7. Radera kurs\n" +
+                            "\n\tA.Avsluta\n");
+                Console.Write("\t----> ");
+                Console.ReadLine();
+
+            }
+
         }
     }
 }
