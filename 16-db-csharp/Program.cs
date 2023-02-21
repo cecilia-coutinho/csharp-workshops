588c89cf5714c45338af1bcd9a639b2e71d4b128
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-02-21 15:08:13 +0100

    cases skeleton funcionality added

--- 16-db-csharp/Program.cs
+++ 16-db-csharp/Program.cs
@@ -29,10 +29,81 @@ namespace Workshop16DbCsharp
                             "\n\t7. Radera kurs\n" +
                             "\n\tA.Avsluta\n");
                 Console.Write("\t----> ");
-                Console.ReadLine();
+
+                int.TryParse(Console.ReadLine(), out int menuChoice);
+
+                switch (menuChoice)
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("coming soon");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("coming soon");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("coming soon");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("coming soon");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                    case 5:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("coming soon");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                    case 6:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("coming soon");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                    case 7:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("coming soon");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                    case 0:
+                        Console.WriteLine("\n\tThanks for your visit!");
+                        Thread.Sleep(1000);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        InvalidOption();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n\tVänligen välj 1-7 från menyn eller A för att logga ut\n");
+                        Console.ResetColor();
+                        GoBackMenuOptions();
+                        break;
+                }
 
             }
+        }
 
+        static void GoBackMenuOptions()
+        {
+            Console.WriteLine("\n\tPress ENTER to go back to the menu.\n");
+            Console.ReadLine();
+        }
+        static void InvalidOption()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n\tERROR: Invalid Option!".ToUpper());
+            Console.ResetColor();
         }
     }
 }
